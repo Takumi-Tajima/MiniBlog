7f4b8fed7f869331f2283e25da6b041a9d410e27
Takumi Tajima <tajima.takumi0421@gmail.com>
2023-11-02 16:45:00 +0900

    run sgcop

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,24 +1,24 @@
+# frozen_string_literal: true
+
 class MicropostsController < ApplicationController
-  before_action :set_micropost, only: %i[ show edit update destroy ]
+  before_action :set_micropost, only: %i[show edit update destroy]
 
   def index
     @microposts = Micropost.all
   end
 
-  def show
-  end
+  def show; end
 
   def new
     @micropost = Micropost.new
   end
 
-  def edit
-  end
+  def edit; end
 
   def create
     @micropost = Micropost.new(micropost_params)
     if @micropost.save
-      redirect_to @micropost, notice: "新規投稿を作成しました。"
+      redirect_to @micropost, notice: '新規投稿を作成しました。'
     else
       render :new, status: :unprocessable_entity
     end
@@ -26,7 +26,7 @@ class MicropostsController < ApplicationController
 
   def update
     if @micropost.update(micropost_params)
-      redirect_to @micropost, notice: "投稿を更新しました。", status: :see_other
+      redirect_to @micropost, notice: '投稿を更新しました。', status: :see_other
     else
       render :edit, status: :unprocessable_entity
     end
@@ -34,15 +34,16 @@ class MicropostsController < ApplicationController
 
   def destroy
     @micropost.destroy
-    redirect_to microposts_url, notice: "投稿を削除しました。", status: :see_other
+    redirect_to microposts_url, notice: '投稿を削除しました。', status: :see_other
   end
 
   private
-    def set_micropost
-      @micropost = Micropost.find(params[:id])
-    end
 
-    def micropost_params
-      params.require(:micropost).permit(:content)
-    end
+  def set_micropost
+    @micropost = Micropost.find(params[:id])
+  end
+
+  def micropost_params
+    params.require(:micropost).permit(:content)
+  end
 end
