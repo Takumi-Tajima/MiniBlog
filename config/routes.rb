e03f926c89a5a4d8fc841c6da2cbb27dacb10a2f
Takumi Tajima <tajima.takumi0421@gmail.com>
2023-11-08 16:34:01 +0900

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@
 
 Rails.application.routes.draw do
   root to: 'microposts#index'
-  devise_for :users
-  resources :users, only: %i[index show]
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   resources :microposts
+  resources :users, only: %i[index show]
 end
