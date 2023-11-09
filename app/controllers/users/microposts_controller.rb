c7367d74426568f5114eb60d48f2ae3b7e644b8c
Takumi Tajima <tajima.takumi0421@gmail.com>
2023-11-09 19:22:11 +0900

    microposts_controllerを分割 ユーザー権限と公開

---
+++ app/controllers/users/microposts_controller.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+class Users::MicropostsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_micropost, only: %i[edit update destroy]
+
+  def new
+    @micropost = Micropost.new
+  end
+
+  def edit; end
+
+  def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      redirect_to @micropost, success: '新規投稿を作成しました。'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @micropost.update(micropost_params)
+      redirect_to @micropost, success: '投稿を更新しました。', status: :see_other
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @micropost.destroy!
+    redirect_to microposts_url, success: '投稿を削除しました。', status: :see_other
+  end
+
+  private
+
+  def set_micropost
+    @micropost = current_user.microposts.find(params[:id])
+  end
+
+  def micropost_params
+    params.require(:micropost).permit(:content)
+  end
+end
