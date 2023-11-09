286fac9a56c0a9f6c06bb63263e8e099706f3991
Takumi Tajima <tajima.takumi0421@gmail.com>
2023-11-09 17:39:03 +0900

    プロフィール編集のMVCを生成

--- app/controllers/users/profiles_controller.rb
+++ app/controllers/users/profiles_controller.rb
@@ -1,7 +1,25 @@
 class Users::ProfilesController < ApplicationController
-  def edit
-  end
+  before_action :set_user
+
+  def show; end
+
+  def edit; end
 
   def update
+    if @user.update(user_params)
+      redirect_to user_profile_path, success: '名前/プロフィールを更新しました', status: :see_other
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(current_user.id)
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :profile)
   end
 end
