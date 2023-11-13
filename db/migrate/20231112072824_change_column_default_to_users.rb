bc2691b4ba6e4186886ba3a0af4a744a0accf966
Takumi Tajima <tajima.takumi0421@gmail.com>
2023-11-13 17:55:45 +0900

    間違えたマイグレファイル削除

--- db/migrate/20231112072824_change_column_default_to_users.rb
+++
@@ -1,7 +0,0 @@
-class ChangeColumnDefaultToUsers < ActiveRecord::Migration[7.0]
-  def change
-    change_column_default :users, :name, from: nil, to: ''
-    change_column_default :users, :email, from: nil, to: ''
-    change_column_default :users, :encrypted_password, from: nil, to: ''
-  end
-end
