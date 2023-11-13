86eede1bd6c69e718d82e0cfb5db3b0bf8d4fd6a
Takumi Tajima <tajima.takumi0421@gmail.com>
2023-11-13 13:47:08 +0900

    user情報のバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
+  validates :name, presence: true, format: { with: /\A[a-z]+\z/, message: "英文字のみが使えます" }, length: { maximum: 20 }
+  validates :email, presence: true
+  validates :profile, length: { maximum: 200 }
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
