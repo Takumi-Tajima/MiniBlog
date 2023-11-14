c84e7d8cbbb46c3011c1f24f11b68fb038bde505
Takumi Tajima <tajima.takumi0421@gmail.com>
2023-11-14 23:18:56 +0900

    モデルデータの生成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    name { Faker::Name.unique.name.gsub(/\s+/, '') }
+    name { 'aaa' }
     email { Faker::Internet.unique.email }
     profile { Faker::Lorem.sentence }
     password { 'foobar' }
