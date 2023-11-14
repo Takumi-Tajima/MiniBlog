4ca4e6d5d6261d4abe538c5375a95728732484f3
Takumi Tajima <tajima.takumi0421@gmail.com>
2023-11-14 10:53:35 +0900

    set_user削除 authenticate_user!の追記

--- app/controllers/users/profiles_controller.rb
+++ app/controllers/users/profiles_controller.rb
@@ -1,12 +1,12 @@
 class Users::ProfilesController < ApplicationController
-  before_action :set_user
+  before_action :authenticate_user!
 
   def show; end
 
   def edit; end
 
   def update
-    if @user.update(user_params)
+    if current_user.update(user_params)
       redirect_to user_profile_path, success: '名前/プロフィールを更新しました', status: :see_other
     else
       render :edit, status: :unprocessable_entity
@@ -14,10 +14,6 @@ class Users::ProfilesController < ApplicationController
   end
 
   private
-
-  def set_user
-    @user = current_user
-  end
 
   def user_params
     params.require(:user).permit(:name, :profile)
