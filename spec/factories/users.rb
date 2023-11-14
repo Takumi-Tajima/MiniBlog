c19f05085be76c120d389628df1c1c57fd115c41
Takumi Tajima <tajima.takumi0421@gmail.com>
2023-11-14 16:37:19 +0900

    FactoryBot userを定義

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    name { Faker::Name.unique.name.gsub(/\s+/, '') }
+    email { Faker::Internet.unique.email }
+    profile { Faker::Lorem.sentence }
+    password { 'foobar' }
   end
 end
