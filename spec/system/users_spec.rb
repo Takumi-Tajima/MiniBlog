4968381290530eefb17a7cbe6d6db9b09d2e1692
Takumi Tajima <tajima.takumi0421@gmail.com>
2023-11-15 13:50:02 +0900

    Rspec ログイン後にheaderのリンク数をテスト

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,23 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :system do
+RSpec.describe 'Users', type: :system do
   let(:user) { FactoryBot.create(:user) }
 
-  context 'ユーザー登録ができるとき' do
-    it '正しい情報を入力すればユーザー新規登録ができてトップページに移動する' do
-      # ユーザーがサイトに訪れる
-      # visit root_path
-      expect(user.name).to eq 'aaa'
-      # ヘッダーに表示されているログインボタンを押す
-      # 登録をしていないのでsing_upをクリック
-      # ユーザー情報を打ち込む
-      # 入力が完了してsing_upボタンを押す
-      # Userモデルのカウント数が1上がる
-      # 投稿の一覧画面（root_path）へリダイレクトされる
-      # headerには、ログアウト、ユーザー一覧、プロフィールの3つが表示されている
-      # ログインボタンは表示されてない
+  context 'ユーザーがログインしている場合' do
+    it 'headerにMiniBlog、ユーザー一覧、プロフィール、ログアウトのリンクの4つが表示される' do
+      sign_in user
+      visit root_path
+      expect(page).to have_selector('nav.navbar a', count: 4)
     end
   end
-
-  pending "add some scenarios (or delete) #{__FILE__}"
 end
