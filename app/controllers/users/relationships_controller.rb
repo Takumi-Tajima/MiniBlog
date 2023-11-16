f26232ecbe32e874ca9e2c13c6b64c1bba0907d3
Takumi Tajima <tajima.takumi0421@gmail.com>
2023-11-16 23:58:37 +0900

    フォロー、フォロー解除 アクションの設定

--- app/controllers/users/relationships_controller.rb
+++ app/controllers/users/relationships_controller.rb
@@ -1,7 +1,15 @@
 class Users::RelationshipsController < ApplicationController
+  before_action :authenticate_user!
+
   def create
+    following = current_user.active_relationships.build(followed_id: params[:user_id])
+    following.save
+    redirect_to request.referer || root_path
   end
 
   def destroy
+    following = current_user.active_relationships.find_by(followed_id: params[:user_id])
+    following.destroy!
+    redirect_to request.referer || root_path
   end
 end
