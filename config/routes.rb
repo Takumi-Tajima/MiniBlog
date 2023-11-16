f52efd38685860a68c95f91f6da3a9a517ac08ff
Takumi Tajima <tajima.takumi0421@gmail.com>
2023-11-16 22:57:14 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
-  namespace :users do
-    get 'relationships/create'
-    get 'relationships/destroy'
-  end
   root to: 'microposts#index'
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
-  resources :microposts, only: %i[index show]
+  resources :microposts, only: %i[index show] do
+    member do
+      get :followings, :followers
+    end
+  end
   resources :users, only: %i[index show]
   resource :user do
     resource :profile, only: %i[show edit update], module: :users
     resources :microposts, only: %i[new edit create update destroy], module: :users
+    resource :relationships, only: %i[create destroy], module: :users
   end
 end
