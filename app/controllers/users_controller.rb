869bed976ee09fb17ce0f261e8c3b8afc7285d39
Takumi Tajima <tajima.takumi0421@gmail.com>
2023-11-16 23:01:22 +0900

    フォロー、フォロワー一覧アクションの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,15 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  # todo - User.find(params[:id])を共通化してもいいかも
+  def followings
+		user = User.find(params[:id])
+		@users = user.followings
+	end
+
+  def followers
+		user = User.find(params[:id])
+		@users = user.followers
+	end
 
 end
