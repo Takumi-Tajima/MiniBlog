3385fb9675d1cb1611af6c36a1ea55f672a27009
Takumi Tajima <tajima.takumi0421@gmail.com>
2023-11-24 16:32:25 +0900

    postsのシステムテストを追記

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe '投稿機能', type: :system do
-  let(:user) { FactoryBot.create(:user) }
+  let!(:user) { FactoryBot.create(:user) }
+  let!(:other_user) { FactoryBot.create(:user) }
+  let!(:micropost) { FactoryBot.create(:micropost, user: user) }
+  let!(:other_micropost) { FactoryBot.create(:micropost, user: other_user) }
   before do
     sign_in user
   end
@@ -14,22 +17,42 @@ RSpec.describe '投稿機能', type: :system do
     expect(page).to have_content 'hogehogehoge'
   end
 
+  it '投稿時に何も入力しないとエラーが発生する' do
+    visit new_user_micropost_path
+    fill_in 'Content', with: ''
+    click_on 'Create Micropost'
+    expect(page).to have_content 'Content は必須項目です。'
+  end
+
   context '自分の投稿のとき' do
     it '編集できること' do
-      visit new_user_micropost_path
-      fill_in 'Content', with: 'hogehogehoge'
-      click_on 'Create Micropost'
+      visit micropost_path(micropost)
+      click_on 'Edit'
+      fill_in 'Content', with: '編集したよ'
+      click_on 'Update Micropost'
+      expect(page).to have_content '編集したよ'
     end
 
-    xit '削除できること' do
+    it '削除できること' do
+      micropost_count_before = Micropost.count
+      visit micropost_path(micropost)
+      expect(page).to have_content 'Destroy'
+      click_on 'Destroy'
+      expect(page).to have_content '投稿を削除しました。'
+      expect(Micropost.count).to eq(micropost_count_before - 1)
+      expect(Micropost.where(id: micropost.id)).to be_empty
     end
   end
 
   context '他人の投稿のとき' do
-    xit '編集できないこと' do
+    it '編集できないこと' do
+      visit micropost_path(other_micropost)
+      expect(page).to_not have_content('Edit')
     end
 
-    xit '削除できないこと' do
+    it '削除できないこと' do
+      visit micropost_path(other_micropost)
+      expect(page).to_not have_content('Destroy')
     end
   end
 end
