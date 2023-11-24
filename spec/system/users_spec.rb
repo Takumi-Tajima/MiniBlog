2b4e280095f281da8d7a5c9299aa6cb6a12a0e3c
Takumi Tajima <tajima.takumi0421@gmail.com>
2023-11-24 16:32:35 +0900

    userのシステムテストを追記

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,13 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :system do
-  let(:user) { FactoryBot.create(:user) }
+  let!(:user) { FactoryBot.create(:user) }
+  let!(:other_user) { FactoryBot.create(:user) }
+  let!(:micropost) { FactoryBot.create(:micropost, user: user) }
 
   context 'ユーザーがログインしている場合' do
-    it 'headerにMiniBlog、ユーザー一覧、プロフィール、ログアウトのリンクの4つが表示される' do
+    before do
       sign_in user
+    end
+    it 'headerにMiniBlog、ユーザー一覧、プロフィール、ログアウトのリンクの4つが表示される' do
       visit root_path
-      expect(page).to have_selector('nav.navbar a', count: 4)
+      expect(page).to have_content('MiniBlog')
+      expect(page).to have_content('ユーザー一覧')
+      expect(page).to have_content('プロフィール')
+      expect(page).to have_content('ログアウト')
+    end
+
+    it 'ユーザー一覧が閲覧できる' do
+      visit users_path
+      expect(page).to have_content('ユーザー一覧')
+    end
+
+    it 'ユーザー情報が閲覧できる' do
+      visit user_path(other_user)
+      expect(page).to have_content('名前')
+      expect(page).to have_content('プロフィール')
+      expect(page).to have_content('フォロー')
+      expect(page).to have_content('フォロワー')
     end
   end
 
@@ -15,6 +35,24 @@ RSpec.describe 'Users', type: :system do
     it 'ユーザー詳細画面にアクセスするとsign in画面にリダイレクトされること' do
       visit user_path(user)
       expect(page).to have_current_path new_user_session_path
+    end
+
+    it 'ユーザー一覧画面にアクセスするとsign in画面にリダイレクトされること' do
+      visit users_path
+      expect(page).to have_current_path new_user_session_path
+    end
+
+    it 'プロフィールにアクセスするとsign in画面にリダイレクトされること' do
+      visit user_profile_path
+      expect(page).to have_current_path new_user_session_path
+    end
+
+    it '投稿の詳細が確認できる' do
+      visit micropost_path(micropost)
+      expect(page).to have_content('内容')
+      expect(page).to have_content('投稿日時')
+      expect(page).to have_content('更新日時')
+      expect(page).to have_content('投稿者')
     end
   end
 end
