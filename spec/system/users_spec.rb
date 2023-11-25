7769474b90ba651410104314c6b1de7edab2d98e
Takumi Tajima <tajima.takumi0421@gmail.com>
2023-11-25 16:31:33 +0900

    rspec userテストをリファクタリング

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Users', type: :system do
   let!(:taro) { FactoryBot.create(:user, name: 'taro') }
   let!(:hanako) { FactoryBot.create(:user, name: 'hanako') }
+  let!(:cocoro) { FactoryBot.create(:user, name: 'cocoro') }
   let!(:taro_micropost) { FactoryBot.create(:micropost, user: taro) }
 
   context 'ユーザーがログインしている場合' do
@@ -11,20 +12,27 @@ RSpec.describe 'Users', type: :system do
     end
     it 'headerにMiniBlog、ユーザー一覧、プロフィール、ログアウトのリンクの4つが表示される' do
       visit root_path
-      expect(page).to have_content('MiniBlog')
-      expect(page).to have_content('ユーザー一覧')
-      expect(page).to have_content('プロフィール')
-      expect(page).to have_content('ログアウト')
+      within('.navbar') do
+        expect(page).to have_content('MiniBlog')
+        expect(page).to have_content('ユーザー一覧')
+        expect(page).to have_content('プロフィール')
+        expect(page).to have_content('ログアウト')
+      end
     end
 
     it 'ユーザー一覧が閲覧できる' do
       visit users_path
       expect(page).to have_content('ユーザー一覧')
+      users = User.all
+      users.each do |user|
+        expect(page).to have_content(user.name)
+      end
+      expect(users.sort_by(&:updated_at)).to eq(users)
     end
 
     it 'ユーザー情報が閲覧できる' do
       visit user_path(hanako)
-      expect(page).to have_content('名前')
+      expect(page).to have_content('hanako')
       expect(page).to have_content('プロフィール')
       expect(page).to have_content('フォロー')
       expect(page).to have_content('フォロワー')
@@ -49,10 +57,10 @@ RSpec.describe 'Users', type: :system do
 
     it '投稿の詳細が確認できる' do
       visit micropost_path(taro_micropost)
-      expect(page).to have_content('内容')
-      expect(page).to have_content('投稿日時')
-      expect(page).to have_content('更新日時')
-      expect(page).to have_content('投稿者')
+      expect(page).to have_content(taro_micropost.content)
+      expect(page).to have_content(I18n.l(taro_micropost.created_at))
+      expect(page).to have_content(I18n.l(taro_micropost.updated_at))
+      expect(page).to have_content(taro_micropost.user.name)
     end
   end
 end
